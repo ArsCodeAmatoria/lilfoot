c35ce95764f10ba7c703d39702ab319f011c65ec
Kojin Fox <kojinfox@gmail.com>
2025-03-13 16:23:16 -0700

    Update byline to link only 'Bigfoot Crane' text to their website

--- ui/byline.tsx
+++ ui/byline.tsx
@@ -5,16 +5,20 @@ export default function Byline() {
     <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
       <div className="rounded-lg bg-black p-3.5 lg:px-5 lg:py-4">
         <div className="flex items-center justify-center text-sm text-gray-400">
-          <a
-            className="flex items-center gap-1.5 text-gray-400 transition-colors hover:text-gray-300 mx-1"
-            href="https://bigfootcrane.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>Made by Bigfoot Crane for </span>
+          <div className="flex items-center gap-1.5 mx-1">
+            <span>Made by </span>
+            <a
+              className="text-gray-400 transition-colors hover:text-gray-300"
+              href="https://bigfootcrane.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Bigfoot Crane
+            </a>
+            <span> for </span>
             <span className="font-['Shadows_Into_Light_Two']">lilfoot</span>
             <PawPrint size={16} className="text-highlight ml-1" />
-          </a>
+          </div>
         </div>
       </div>
     </div>
