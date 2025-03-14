22091dcfb0ec24c5ad0634f5147e7666add480cc
Kojin Fox <kojinfox@gmail.com>
2025-03-13 18:02:40 -0700

    Fix Vercel build: Add missing Tailwind plugins module

---
+++ custom-tailwind-plugins/empty-plugins.js
@@ -0,0 +1,8 @@
+/**
+ * Empty Tailwind plugins module to satisfy build requirements
+ */
+
+module.exports = {
+  typography: require('@tailwindcss/typography'),
+  forms: require('@tailwindcss/forms'),
+};
